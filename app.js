c7a80c3c3f263d7b986d82e7e222bf79f4b3ae7c
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-05-11 22:44:40 +0530

    Added new routes and setted up form data fetching

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 const { engine } = require('express-handlebars')
+const fileUpload=require('express-fileupload')
 
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload())
 
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
--- routes/admin.js
+++ routes/admin.js
@@ -33,4 +33,15 @@ router.get('/', function(req, res, next) {
   res.render('admin/view-products',{admin:true,products})
 });
 
+router.get('/add-product', (req, res) => {
+    res.render('admin/add-product');
+});
+router.post('/add-product',(req,res)=>{
+console.log(req.body)
+console.log(req.files.image)
+res.send("Recieved data.")
+
+})
+
+
 module.exports = router;
