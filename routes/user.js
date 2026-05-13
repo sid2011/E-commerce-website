afee0794c7d0bf2adc51384b2ac8bd9e99aefb7f
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-05-13 21:37:51 +0530

    Added login page

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,14 @@ const productHelper=require('../helpers/product-helpers');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   productHelper.getAllProducts().then((products)=>{
-  res.render('index', {admin:true,products});
+  res.render('index', {products});
   }
 )});
+router.get('/login',(req,res)=>{
+  res.render('../views/login/login-page')
+})
+router.get('/signup',(req,res)=>{
+  res.render('../views/login/signup-page')
+})
 
 module.exports = router;
